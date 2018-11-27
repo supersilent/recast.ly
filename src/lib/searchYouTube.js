5149e9ac879882c6a98a48e74626a511f2c623e1
supersilent <shrikethrush@gmail.com>
2018-11-27 12:10:44 -0800

    sends Get request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  let data = $.get("https://www.googleapis.com/youtube/v3/search", {
+    part: "snippet",
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    type: "video"
+  }).done(function(data) {
+    callback(data.items);
+  });
+  // .done((data) => callback(data.responseJSON.items))
+  // console.log(data);
 };
 
 export default searchYouTube;
